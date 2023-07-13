bc555ff7098566efd73fa25b714740ebfd05fba6
maith96 <threedaniel385@gmail.com>
2023-07-13 19:53:26 +0300

    Add file description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+* main - Entry point of program
+* Description: Using puts
+* Return: Always 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+* main -Entry point
+* Description: using printf
+*
+* Return: Always 0 (Succesful)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
