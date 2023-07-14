930d416d2968588767bec7086c1a21d93e05647d
maith96 <threedaniel385@gmail.com>
2023-07-14 17:26:25 +0300

    print different combinattions of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - prints combinations of two digit numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			if (m == n || n > m)
+				continue;
+
+			putchar(48 + n);
+			putchar(48 + m);
+
+			if (m != 9 && n != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
