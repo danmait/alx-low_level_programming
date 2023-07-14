73e50b2dc07543c85387a3d29de43971dd434f69
maith96 <threedaniel385@gmail.com>
2023-07-14 15:56:09 +0300

    print alhabet in lower and uppercase:

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabet in lower then upercase
+* Return: return 0
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
