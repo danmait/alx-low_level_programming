f675297af0ec6a53fb28cbd96bf7f946ac4c06a7
maith96 <threedaniel385@gmail.com>
2023-08-04 14:56:09 +0300

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - printsnumber of arguments passed to  this programs name
+* @argc: number of arguments
+* @argv: anaarray with all arguments
+* Return: - Zero
+*/
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
+
